96763519bd384c5cc1593b516270458d9cbcceef
Fred Chu <iamfredchu@gmail.com>
2015-11-27 23:08:09 +0800

    added jshintrc and jscs config, updated babelrc, refactored syntax

--- scripts/main.jsx
+++ scripts/main.jsx
@@ -1,16 +1,19 @@
 var TodoList = React.createClass({
-    getInitialState: function () {
+    getInitialState: function() {
+        let list = [
+            'jQuery',
+            'ReactJS',
+            'AngularJS',
+            'Vue.js',
+            'Array.observe()'
+        ];
+
         return {
-            list: [
-                'jQuery',
-                'ReactJS',
-                'AngularJS',
-                'Vue.js',
-                'Array.observe()'
-            ]
+            list: list
         };
     },
-    addTodo: function (e){
+
+    addTodo: function(e) {
         e.preventDefault();
         var todoCotent = ReactDOM.findDOMNode(this.refs.todoCotent).value.trim();
 
@@ -25,6 +28,7 @@ var TodoList = React.createClass({
         this.setState({list: list});
         ReactDOM.findDOMNode(this.refs.todoCotent).value = '';
     },
+
     render: function() {
         // Code here will be linted with JSHint.
         /* jshint ignore:start */
@@ -32,7 +36,7 @@ var TodoList = React.createClass({
             <div>
                 <h1>Todos count: <span>{this.state.list.length}</span></h1>
                 <ul>
-                    {this.state.list.map(function (todo) {
+                    {this.state.list.map(function(todo) {
                        return (
                          <li key={todo.id}>{todo}</li>
                        );
@@ -44,15 +48,18 @@ var TodoList = React.createClass({
                 </form>
             </div>
         );
+
         // Code here will be ignored by JSHint.
         /* jshint ignore:end */
     }
 });
 
 ReactDOM.render(
+
     // Code here will be linted with JSHint.
     /* jshint ignore:start */
     <TodoList />,
+
     // Code here will be ignored by JSHint.
     /* jshint ignore:end */
     document.getElementById('wrap')
