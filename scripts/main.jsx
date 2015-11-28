141e23379c09aadb49824438383ef89114c95042
Fred Chu <iamfredchu@gmail.com>
2015-11-28 20:02:18 +0800

    refactored

--- scripts/main.jsx
+++ scripts/main.jsx
@@ -1,5 +1,62 @@
-var TodoList = React.createClass({
-    getInitialState: function() {
+(function() {
+    var TodoList = React.createClass({
+        getInitialState: function() {
+            return {
+                list: this.props.data.list,
+                currentTyping: this.props.data.currentTyping,
+                passedChildren: this.props.children
+            };
+        },
+
+        addTodo: function(e) {
+            e.preventDefault();
+            var todoContent = ReactDOM.findDOMNode(this.refs.todoContent).value.trim();
+
+            if (!todoContent) {
+              return;
+            }
+
+            var list = this.state.list;
+
+            list.push(todoContent);
+
+            this.setState({list: list});
+            ReactDOM.findDOMNode(this.refs.todoContent).value = '';
+        },
+
+        showCurrentTyping: function(e) {
+            this.setState({currentTyping: e.target.value || 'Type some text YEAH'});
+        },
+
+        render: function() {
+            // Code here will be linted with JSHint.
+            /* jshint ignore:start */
+            return (
+                <div>
+                    <h1>Todos count: <span>{this.state.list.length}</span></h1>
+                    <h2>You're typing: {this.state.currentTyping}</h2>
+                    <ul>
+                        {this.state.list.map(function(todo) {
+                           return (
+                             <li key={todo.id}>{todo}</li>
+                           );
+                        })}
+                    </ul>
+                    <form action="" onSubmit={this.addTodo} onChange={this.showCurrentTyping}>
+                        <input type="text" ref="todoContent" />
+                        <input type="submit" value="Add a Todo" />
+                    </form>
+                    <div>{this.props.testStr}</div>
+                    <div>{this.state.passedChildren}</div>
+                </div>
+            );
+
+            // Code here will be ignored by JSHint.
+            /* jshint ignore:end */
+        }
+    });
+
+    (function() {
         let list = [
             'jQuery',
             'ReactJS',
@@ -10,69 +67,20 @@ var TodoList = React.createClass({
 
         let currentTyping = 'Type some text YEAH';
 
-        return {
+        let data = {
             list: list,
-            currentTyping: currentTyping,
-            passedChildren: this.props.children
+            currentTyping: currentTyping
         };
-    },
 
-    addTodo: function(e) {
-        e.preventDefault();
-        var todoContent = ReactDOM.findDOMNode(this.refs.todoContent).value.trim();
+        ReactDOM.render(
 
-        if (!todoContent) {
-          return;
-        }
+            // Code here will be linted with JSHint.
+            /* jshint ignore:start */
+            <TodoList data={data}>Just testing children then transformed to state</TodoList>,
 
-        var list = this.state.list;
-
-        list.push(todoContent);
-
-        this.setState({list: list});
-        ReactDOM.findDOMNode(this.refs.todoContent).value = '';
-    },
-
-    showCurrentTyping: function(e) {
-        this.setState({currentTyping: e.target.value || 'Type some text YEAH'});
-    },
-
-    render: function() {
-        // Code here will be linted with JSHint.
-        /* jshint ignore:start */
-        return (
-            <div>
-                <h1>Todos count: <span>{this.state.list.length}</span></h1>
-                <h2>You're typing: {this.state.currentTyping}</h2>
-                <ul>
-                    {this.state.list.map(function(todo) {
-                       return (
-                         <li key={todo.id}>{todo}</li>
-                       );
-                    })}
-                </ul>
-                <form action="" onSubmit={this.addTodo} onChange={this.showCurrentTyping}>
-                    <input type="text" ref="todoContent" />
-                    <input type="submit" value="Add a Todo" />
-                </form>
-                <div>{this.props.testStr}</div>
-                <div>{this.props.testNum}</div>
-                <div>{this.state.passedChildren}</div>
-            </div>
+            // Code here will be ignored by JSHint.
+            /* jshint ignore:end */
+            document.getElementById('wrap')
         );
-
-        // Code here will be ignored by JSHint.
-        /* jshint ignore:end */
-    }
-});
-
-ReactDOM.render(
-
-    // Code here will be linted with JSHint.
-    /* jshint ignore:start */
-    <TodoList testNum={111}>Just testing children then transformed to state</TodoList>,
-
-    // Code here will be ignored by JSHint.
-    /* jshint ignore:end */
-    document.getElementById('wrap')
-);
+    }());
+}());
